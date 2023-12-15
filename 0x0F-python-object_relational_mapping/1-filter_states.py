5356b2dc98535c5e6cdb55da7384b59294d0d1b1
MohamedElgamal19 <115739856+MohamedElgamal19@users.noreply.github.com>
2023-12-15 20:00:17 +0200

    Add files via upload
    alx

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-"""
-This script lists all states with
-a `name` starting with the letter `N`
-from the database `hbtn_0e_0_usa`.
-"""
-
-import MySQLdb as db
+"""script that lists all states with a name starting with N (upper N) from
+the database hbtn_0e_0_usa"""
+import MySQLdb
 from sys import argv
 
-"""
-Access to the database and get the states
-from the database.
-"""
-
-if __name__ == '__main__':
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-    db_cursor = db_connect.cursor()
-
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE BINARY 'N%' \
-                ORDER BY states.id ASC")
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,17 @@
 #!/usr/bin/python3
-"""
-This script  takes in the name of a state
-as an argument and lists all cities of that
-state, using the database `hbtn_0e_4_usa`.
-"""
-
-import MySQLdb as db
+"""Script that takes in the name of a state as an argument and lists
+all cities of that state, using the database hbtn_0e_4_usa"""
+import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
-
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("""
-            SELECT
-                cities.id, cities.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            WHERE
-                states.name LIKE BINARY %(state_name)s
-            ORDER BY
-                cities.id ASC
-        """, {
-            'state_name': argv[4]
-        })
-        rows_selected = db_cursor.fetchall()
-
-    if rows_selected is not None:
-        print(", ".join([row[1] for row in rows_selected]))
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (argv[4],))
+    rows = cur.fetchall()
+    print(", ".join(city[0] for city in rows))
+    cur.close()
+    db.close()
